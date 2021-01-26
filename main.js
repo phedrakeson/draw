699a0c0e793610f02b7119e9122687a1e45af6f4
phedrakeson <gabrield.developer@gmail.com>
2021-01-26 19:31:59 -0300

    feat: experimental drawings with canvas

--- main.js
+++ main.js
@@ -16,7 +16,7 @@
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_sketch_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/sketch.js */ \"./scripts/modules/sketch.js\");\n\n\nconst sketch = new _modules_sketch_js__WEBPACK_IMPORTED_MODULE_0__.default();\n\nsketch.setup();\n\n\n//# sourceURL=webpack://draw/./scripts/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_sketch_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/sketch.js */ \"./scripts/modules/sketch.js\");\n\n\nconst sketch = new _modules_sketch_js__WEBPACK_IMPORTED_MODULE_0__.default('sketch');\n\nsketch.draw();\n\n\n//# sourceURL=webpack://draw/./scripts/main.js?");
 
 /***/ }),
 
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mod
   \***********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => /* binding */ Sketch\n/* harmony export */ });\nclass Sketch {\n  constructor() {\n\n  }\n\n  setup() {\n\n  }\n  \n}\n\n//# sourceURL=webpack://draw/./scripts/modules/sketch.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => /* binding */ Sketch\n/* harmony export */ });\nclass Sketch {\n  constructor(canvas) {\n    this.canvas = document.getElementById(canvas)\n    this.context = this.canvas.getContext('2d')\n  }\n\n  setup() {\n\n  }\n\n  draw() {\n    // this.context.fillStyle = \"rgb(200,0,0)\"\n    // this.context.fillRect (10, 10, 55, 50);\n\n    // this.context.fillStyle = \"rgba(0, 0, 200, 0.5)\";\n    // this.context.fillRect (30, 30, 55, 50);\n\n    // this.context.fillRect(25, 25, 100, 100)\n    // this.context.clearRect(45, 45, 60, 60)\n    // this.context.strokeRect(50, 50, 50, 50)\n\n\n    this.context.beginPath()\n    this.context.moveTo(200, 300)\n    this.context.lineTo(220, 325)\n    this.context.lineTo(220, 275)\n    this.context.fill()\n\n\n    this.context.beginPath();\n    this.context.arc(75, 75, 50, 0, Math.PI * 2, true); // Círculo exterior\n    this.context.moveTo(110, 75);\n    this.context.arc(75, 75, 35, 0, Math.PI, false);  // Boca (sentido horário)\n    this.context.moveTo(65, 65);\n    this.context.arc(60, 65, 5, 0, Math.PI * 2, true);  // Olho esquerdo\n    this.context.moveTo(95, 65);\n    this.context.arc(90, 65, 5, 0, Math.PI * 2, true);  // Olho direito\n    this.context.stroke();\n\n     // Stroked triangle\n     this.context.beginPath();\n     this.context.moveTo(125,125);\n     this.context.lineTo(125,45);\n     this.context.lineTo(45,125);\n     this.context.closePath();\n     this.context.stroke();\n\n     this.context.beginPath()\n     this.context.moveTo(200,200)\n     this.context.lineTo(200, 300)\n     this.context.lineTo(200, 400)\n     this.context.stroke()\n  }\n  \n}\n\n//# sourceURL=webpack://draw/./scripts/modules/sketch.js?");
 
 /***/ })
 
--- scripts/modules/sketch.js
+++ scripts/modules/sketch.js
@@ -1,10 +1,55 @@
 export default class Sketch {
-  constructor() {
-
+  constructor(canvas) {
+    this.canvas = document.getElementById(canvas)
+    this.context = this.canvas.getContext('2d')
   }
 
   setup() {
 
+  }
+
+  draw() {
+    // this.context.fillStyle = "rgb(200,0,0)"
+    // this.context.fillRect (10, 10, 55, 50);
+
+    // this.context.fillStyle = "rgba(0, 0, 200, 0.5)";
+    // this.context.fillRect (30, 30, 55, 50);
+
+    // this.context.fillRect(25, 25, 100, 100)
+    // this.context.clearRect(45, 45, 60, 60)
+    // this.context.strokeRect(50, 50, 50, 50)
+
+
+    this.context.beginPath()
+    this.context.moveTo(200, 300)
+    this.context.lineTo(220, 325)
+    this.context.lineTo(220, 275)
+    this.context.fill()
+
+
+    this.context.beginPath();
+    this.context.arc(75, 75, 50, 0, Math.PI * 2, true); // Círculo exterior
+    this.context.moveTo(110, 75);
+    this.context.arc(75, 75, 35, 0, Math.PI, false);  // Boca (sentido horário)
+    this.context.moveTo(65, 65);
+    this.context.arc(60, 65, 5, 0, Math.PI * 2, true);  // Olho esquerdo
+    this.context.moveTo(95, 65);
+    this.context.arc(90, 65, 5, 0, Math.PI * 2, true);  // Olho direito
+    this.context.stroke();
+
+     // Stroked triangle
+     this.context.beginPath();
+     this.context.moveTo(125,125);
+     this.context.lineTo(125,45);
+     this.context.lineTo(45,125);
+     this.context.closePath();
+     this.context.stroke();
+
+     this.context.beginPath()
+     this.context.moveTo(200,200)
+     this.context.lineTo(200, 300)
+     this.context.lineTo(200, 400)
+     this.context.stroke()
   }
   
 }
