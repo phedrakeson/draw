4a1c79b932b5da334729185236115d4f269e9426
phedrakeson <gabrield.developer@gmail.com>
2021-01-26 21:48:05 -0300

    feat: mobile drawing recognition

--- scripts/modules/sketch.js
+++ scripts/modules/sketch.js
@@ -8,6 +8,13 @@ export default class Sketch {
     this.y = undefined;
 
     this.ongoingTouches = new Array;
+
+    this.handleStart = this.handleStart.bind(this);
+    this.handleEnd = this.handleEnd.bind(this);
+    this.handleMove = this.handleMove.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.ongoingTouchIndexById = this.ongoingTouchIndexById.bind(this);
+    this.copyTouch = this.copyTouch.bind(this)
   }
 
   mouseRecognitions() {
@@ -43,7 +50,7 @@ export default class Sketch {
     this.canvas.addEventListener('touchend', this.handleEnd, false);
     this.canvas.addEventListener('touchmove', this.handleMove, false);
     this.canvas.addEventListener('touchcancel', this.handleCancel, false);
-    this.canvas.addEventListener('touchleave', this.handleLeave, false)
+    this.canvas.addEventListener('touchleave', this.handleEnd, false)
   }
 
   // Mobile drawing handlers
@@ -54,9 +61,6 @@ export default class Sketch {
 
     for(let i = 0; i < touches.length; i++) {
       this.ongoingTouches.push(this.copyTouch(touches[i]));
-      this.context.beginPath();
-      this.context.arc(touches[i].pageX, touches[i].pageY, 4, 0,2*Math.PI, false);
-      this.context.fill()
     }
   }
 
@@ -67,11 +71,13 @@ export default class Sketch {
 
     for(let i = 0; i < touches.length; i++) {
       const idx = this.ongoingTouchIndexById(touches[i].identifier);
-      if(idx >= 0) {
-        this.context.beginPath();
-        this.context.moveTo(this.ongoingTouches[idx].pageX, this.ongoingTouches[idx].pageY);
-        this.context.stroke();
+      this.x = this.ongoingTouches[idx].pageX;
+      this.y = this.ongoingTouches[idx].pageY;
 
+      const x2 = touches[i].pageX;
+      const y2 = touches[i].pageY;
+      if(idx >= 0) {
+        this.drawLine(this.x, this.y, x2, y2)
         this.ongoingTouches.splice(idx, 1, this.copyTouch(touches[i]));
       } else {
         console.error("Can't figure out which touch to continue.")
@@ -87,14 +93,35 @@ export default class Sketch {
       const idx = this.ongoingTouchIndexById(touches[i].identifier);
       if(idx >= 0) {
         this.context.beginPath();
-        this.context.moveTo(this.ongoingTouches[idx].pageX, this.ongoingTouches[idx].pageY);
-        this.context.lineTo(touches[i].pageX, touches[i].pageY);
-        this.context.fillRect(touches[i].pageX-4, touches[i].pageY-4, 8, 8);
         this.ongoingTouches.splice(idx, 1);
       } else {
         console.error("Can't figure out which touch to end.")
       }
     }
+  }
+
+  handleCancel(event) {
+    event.preventDefault();
+
+    const touches = event.changedTouches;
+
+    for(let i = 0; i < touches.length; i++) 
+      this.ongoingTouches.splice(i, 1);
+  }
+
+  copyTouch(touch) {
+    return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
+  }
+
+  ongoingTouchIndexById(idToFind) {
+    for (var i=0; i < this.ongoingTouches.length; i++) {
+      var id = this.ongoingTouches[i].identifier;
+  
+      if (id == idToFind) {
+        return i;
+      }
+    }
+    return -1;
   }
 
 
