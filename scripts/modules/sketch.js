0f20be30a4f0dcefc1d16891c1208cb44a03997f
Renan Penha Marquette <rpenhamarquette@gmail.com>
2021-06-06 17:38:39 -0300

    Undo system + Keymap

--- scripts/modules/sketch.js
+++ scripts/modules/sketch.js
@@ -3,11 +3,16 @@ export default class Sketch {
     this.canvas = document.getElementById(canvas)
     this.context = this.canvas.getContext('2d')
     this.pressed = false;
-    
     this.x = undefined;
     this.y = undefined;
+    this.lineWidthLimit = 26;
     this.ongoingTouches = new Array;
-
+    this.states = []
+    this.color = "#000000"
+    
+    this.undo_handle = this.undo_handle.bind(this);
+    this.decreaseBtn_handle = this.decreaseBtn_handle.bind(this);
+    this.increaseBtn_handle = this.increaseBtn_handle.bind(this);
     this.handleStart = this.handleStart.bind(this);
     this.handleEnd = this.handleEnd.bind(this);
     this.handleMove = this.handleMove.bind(this);
@@ -16,13 +21,53 @@ export default class Sketch {
     this.copyTouch = this.copyTouch.bind(this);
     this.adjustLine = this.adjustLine.bind(this);
 
-
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
+
+    window.onkeydown = e => {
+      switch (e.key) {
+        case "i":
+          document.getElementById('modalInfo').classList.toggle("desappear");
+          break;
+
+        case "e":
+          this.toggleTools();
+          break;
+
+        case "r":
+          this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+          break;
+
+        case "a":
+          this.decreaseBtn_handle();
+          break;
+
+        case "d":
+          this.increaseBtn_handle();
+          break;
+
+        case "z":
+          this.undo_handle();
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  undo_handle() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.states.map((draw, index) => {
+      if (index < this.states.length - 1) {
+        this.drawCircle(draw.x, draw.y, draw.size, draw.colorStyle)
+        this.drawLine(draw.x, draw.y, draw.prevX, draw.prevY, draw.size, draw.colorStyle);
+      } else this.states.pop();
+    })
   }
 
   mouseRecognitions() {
-    this.canvas.addEventListener('mousedown', (event) => {
+    this.canvas.addEventListener('mousedown', event => {
       this.pressed = true;
       this.x = event.offsetX;
       this.y = event.offsetY;
@@ -42,11 +87,11 @@ export default class Sketch {
           const sizeFactor = 4;
           const size = this.value * sizeFactor;
           const centralize = pos => pos - size / 2; // Returns center position value based on eraser size
-
           this.context.clearRect(centralize(x2),  centralize(y2), size, size);
         } else {
-          this.drawCircle(x2, y2)
-          this.drawLine(this.x, this.y, x2, y2);
+          this.drawCircle(x2, y2, this.value, this.color)
+          this.drawLine(this.x, this.y, x2, y2, this.value, this.color);
+          this.states.push({x: x2, y: y2, prevX: this.x, prevY: this.y, size: this.value, colorStyle: this.color});
           this.x = x2;
           this.y = y2;
         }
@@ -151,26 +196,26 @@ export default class Sketch {
     this.setUpTools();
   }
 
+  decreaseBtn_handle() {
+    this.value -= 2;
+    if(this.value <= 0) this.value = 2;
+    this.updateWidthValueOnScreen();
+  }
+
+  increaseBtn_handle() {
+    this.value += 2;
+    if(this.value >= this.lineWidthLimit) this.value = this.lineWidthLimit;
+    this.updateWidthValueOnScreen();
+  }
+
   adjustLine() {
     this.decreaseBtn = document.getElementById('decrease');
     this.increaseBtn = document.getElementById('increase');
     this.lineWidthElement = document.getElementById('widthValue');
     this.value = 2
-    this.lineWidthElement.innerText = this.value
-
-    this.decreaseBtn.addEventListener('click', () => {
-      this.value -= 2;
-      if(this.value <= 0) this.value = 2;
-      this.lineWidthElement.innerText = this.value;
-      this.updateWidthValueOnScreen(this.value);
-    });
-
-    this.increaseBtn.addEventListener('click', () => {
-      this.value += 2;
-      if(this.value >= 20) this.value = 20;
-      this.lineWidthElement.innerText = this.value;
-      this.updateWidthValueOnScreen(this.value);
-    })
+    this.updateWidthValueOnScreen();
+    this.decreaseBtn.addEventListener('click', this.decreaseBtn_handle);
+    this.increaseBtn.addEventListener('click', this.increaseBtn_handle)
   }
 
   adjustColors() {
@@ -178,35 +223,33 @@ export default class Sketch {
     document.getElementById('color').addEventListener('change', event => this.color = event.target.value);
   }
 
-  updateWidthValueOnScreen(value) {
-    this.lineWidthElement.innerText = value;
+  updateWidthValueOnScreen() {
+    this.lineWidthElement.innerText = this.value;
   }
 
   // Drawing methods
 
-  drawLine(x1, y1, x2, y2) {
-    this.context.lineWidth = this.lineWidthElement.innerText * 2;
+  drawLine(x1, y1, x2, y2, size, color) {
+    this.context.lineWidth = size * 2;
     this.context.beginPath();
     this.context.moveTo(x1, y1);
     this.context.lineTo(x2,y2);
-    this.context.strokeStyle = this.color
+    this.context.strokeStyle = color
     this.context.stroke()
   }
 
-  drawCircle(x, y) {
+  drawCircle(x, y, size, color) {
     this.context.beginPath()
-    this.context.arc(x, y, this.lineWidthElement.innerText, 0, Math.PI * 2);
-    this.context.fillStyle = this.color;
+    this.context.arc(x, y, size, 0, Math.PI * 2);
+    this.context.fillStyle = color;
     this.context.fill()
   }
   
   setUpTools() {
     document.getElementById('trash').addEventListener('click', () => this.context.clearRect(0, 0, this.canvas.width, this.canvas.height));
+    document.getElementById('undo').addEventListener('click', () => this.undo_handle())
     document.getElementById('eraser').addEventListener('click', () => this.toggleTools())
-    document.getElementById('pencil').addEventListener('click', () => {
-      this.color = document.getElementById('color').value;
-      this.toggleTools();
-    })
+    document.getElementById('pencil').addEventListener('click', () => this.toggleTools())
   }
 
   toggleTools() {
